fb906568f4c1cd5b3acded405f7a91c1bd5d935e
Matt Andrews <matthew.andrews@guardian.co.uk>
2015-01-07 16:08:51 +0000

    start mapping over country codes

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
+var worldCountries = require('world-countries');
 var Converter = require("csvtojson").core.Converter;
 var fs = require("fs");
+var _ = require("underscore");
 
 var csvConverter = new Converter({
     constructResult: true
 });
 
 csvConverter.on("end_parsed", function(jsonObj) {
-    console.log(jsonObj);
     fs.writeFile("./GDP.json", JSON.stringify(jsonObj), function (err) {
-        if (err) {
-            throw err;
-        }
+        if (err) { throw err; }
         console.log("file was saved");
     });
+
+    geocode(jsonObj);
 });
 
-var csvFileName = "./GDP-clean.csv";
-var fileStream = fs.createReadStream(csvFileName);
-fileStream.pipe(csvConverter);
+var makeJson = function () {
+    var csvFileName = "./GDP-clean.csv";
+    var fileStream = fs.createReadStream(csvFileName);
+    fileStream.pipe(csvConverter);
+};
+
+var geocode = function (GDP) {
+
+    var data = fs.readFile("./node_modules/world-countries/dist/countries.json", function(err, data) {
+        if (err) { throw err; }
+        var countries = JSON.parse(data);
+
+        GDP.map(function(item) {
+            var row = _.find(countries, function(c) {
+                return c.cca3 === item.code;
+            });
+            if (!row) {
+                console.log("no match for " + item.name);
+            } else {
+                // console.log(row.cca3, item.name);
+            }
+        })
+    })
+};
+
+makeJson();
